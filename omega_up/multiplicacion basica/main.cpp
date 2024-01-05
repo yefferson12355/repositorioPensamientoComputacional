9f9bcb50c47014a6d1c07500683fcc14ca7c54d6
yefferson12355 <148633201+yefferson12355@users.noreply.github.com>
2024-01-05 09:44:57 -0500

    Add files via upload
    problemas de omega_up ...

---
+++ omega_up/multiplicacion basica/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+/* Entrada
+Pide dos numeros enteros
+
+Salida
+mostrar el resultado de la multiplicacion de los numeros pedidos*/
+int main()
+{
+    int num1,num2,resultado;
+    cin>>num1;
+    cin>>num2;
+    resultado=num1*num2;
+    cout <<resultado<< endl;
+    return 0;
+}
