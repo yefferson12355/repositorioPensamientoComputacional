04c19fc54f5abe5ebba7878c7089474ea5ffc4e7
yefferson12355 <148633201+yefferson12355@users.noreply.github.com>
2024-01-07 22:54:54 -0500

    Add files via upload

---
+++ omega_up/hackermen_te_reta/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //entrada de tres numeros
+    float x,y,z;
+    cin>>x>>y>>z;
+    float suma;
+    suma=x+y+z;
+    cout<<suma;
+    return 0;
+}
