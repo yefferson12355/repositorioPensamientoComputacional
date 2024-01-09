3e9a20e13911ad4a681a9ca5060b5c11377ae282
yefferson12355 <148633201+yefferson12355@users.noreply.github.com>
2024-01-08 22:17:38 -0500

    Add files via upload

---
+++ omega_up/Comparaciones/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int c,i,n,canguros,comida,ini,fin;
+int cola[1000002];
+char accion;
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    cin>>c>>n;
+    ini=1; fin=1;
+    while(n>=1){
+        cin>>accion;
+        if(accion=='N'){
+            cin>>canguros; canguros++;
+            cola[fin]=canguros; fin++;
+        }else if(accion=='A'){
+            c-=cola[ini]; ini++;
+        }else if(accion=='R'){
+            cout<<c<<'\n';
+        }else if(accion=='C'){
+            cout<<fin-ini<<'\n';
+        }
+        n--;
+    }
+    return 0;
+}
+/// xd
